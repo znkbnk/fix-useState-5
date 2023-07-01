791514eab6c6784d80ad265f2dc6c8c12bb8488d
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-07-01 16:31:09 +0100

    Update incorrect.js

--- incorrect.js
+++ incorrect.js
@@ -6,11 +6,9 @@ function ShoppingCart() {
   const [items, setItems] = useState([]);
 
   function addToCart(product) {
-    // Mistake 2: Incorrect usage of the spread operator
     setItems(...items, product);
   }
 
-  // Mistake 3: Missing parentheses in the map function
   const renderedItems = items.map(item => {
     return <li>[item]</li>;
   });
